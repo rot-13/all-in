6ef42396f1024b472c0230a282e7c9e699e23964
yon <Lighthawky@gmail.com>
2015-10-07 18:02:16 +0300

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-
   # Omniauth
   get 'auth/facebook/callback', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
 
-=======
   root 'application#root'
->>>>>>> master
 end
