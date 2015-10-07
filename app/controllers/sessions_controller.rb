81528201ebc38929f37bd000931277f89de56ec4
Assaf Gelber <assaf.gelber@gmail.com>
2015-10-07 17:59:20 +0300

    Add option to add redirect_url to session after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,11 @@ class SessionsController < ApplicationController
   def create
     user = User.facebook(env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to root_url
+    redirect_to session[:redirect_url] || root_url
   end
 
   def destroy
     session[:user_id] = nil
+    redirect_to root_url
   end
 end
