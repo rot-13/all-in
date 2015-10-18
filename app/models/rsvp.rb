72bfd78f09779297036a167369e6ddc26098ffa1
Assaf Gelber <assaf.gelber@gmail.com>
2015-10-18 17:36:15 +0300

    First attempt at pusher

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -5,4 +5,8 @@ class Rsvp < ActiveRecord::Base
   enum response: [:in, :out]
 
   before_update { |rsvp| rsvp.attendees = 1 if rsvp.out? }
+
+  after_save do |rsvp|
+    Pusher.trigger("event_#{rsvp.event.token}", 'change')
+  end
 end
