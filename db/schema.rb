f0de3c1c7be75e50d3242903b458288b594b9e21
yon <Lighthawky@gmail.com>
2015-10-18 17:35:48 +0300

    date field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007134617) do
+ActiveRecord::Schema.define(version: 20151018143211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151007134617) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "date"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
